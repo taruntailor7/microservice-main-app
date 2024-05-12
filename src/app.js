4dc63a1c7da5ba39e91da593f9b38e5e0211978a
Tarun Tailor <taruntailor7@gmail.com>
2024-05-12 14:48:15 +0530

    RabbitMQ Connectioncode added but in rmq serverhas some issue i guess so right now not able to establish connection but going forward by writing code for establishing communication between admin and main app microservices

--- src/app.js
+++ src/app.js
@@ -3,12 +3,26 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var cors = require("cors");
 var typeorm_1 = require("typeorm");
+var amqp = require("amqplib/callback_api");
 (0, typeorm_1.createConnection)().then(function (db) {
-    var app = express();
-    app.use(cors({
-        origin: ["http://localhost:3000", "http://localhost:8080", "http://localhost:4200"]
-    }));
-    app.use(express.json());
-    console.log("Listening on port: 8001");
-    app.listen(8001);
+    // amqp://ybbxuunk:Pmp2KpIcWnLPIt5VPqviv34pJDvyXWOy@rabbitmq-rpeg.onrender.com:5672
+    amqp.connect('amqps://ybbxuunk:Pmp2KpIcWnLPIt5VPqviv34pJDvyXWOy@puffin.rmq2.cloudamqp.com/ybbxuunk', function (error0, connection) {
+        if (error0) {
+            console.log("Error0: ", error0);
+            throw error0;
+        }
+        connection.createChannel(function (error1, channel) {
+            if (error1) {
+                console.log("Error1: ", error1);
+                throw error1;
+            }
+            var app = express();
+            app.use(cors({
+                origin: ["http://localhost:3000", "http://localhost:8080", "http://localhost:4200"]
+            }));
+            app.use(express.json());
+            console.log("Listening on port: 8001");
+            app.listen(8001);
+        });
+    });
 });
--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,32 @@ import * as express from "express";
 import { Request, Response} from "express";
 import * as cors from "cors";
 import { createConnection } from "typeorm";
-
+import * as amqp from 'amqplib/callback_api';
 
 createConnection().then(db => {
-    const app = express();
-    
-    app.use(cors({
-        origin: ["http://localhost:3000", "http://localhost:8080", "http://localhost:4200"]
-    }))
-    
-    app.use(express.json());
-    
-    console.log("Listening on port: 8001"); 
-    app.listen(8001);
+    // amqp://ybbxuunk:Pmp2KpIcWnLPIt5VPqviv34pJDvyXWOy@rabbitmq-rpeg.onrender.com:5672
+    amqp.connect('amqps://ybbxuunk:Pmp2KpIcWnLPIt5VPqviv34pJDvyXWOy@puffin.rmq2.cloudamqp.com/ybbxuunk', (error0, connection) => {
+        if(error0) {
+            console.log("Error0: ", error0); 
+            throw error0;
+        }
+
+        connection.createChannel((error1, channel) => {
+            if(error1) {
+                console.log("Error1: ", error1); 
+                throw error1;
+            }
+
+            const app = express();
+            
+            app.use(cors({
+                origin: ["http://localhost:3000", "http://localhost:8080", "http://localhost:4200"]
+            }))
+            
+            app.use(express.json());
+            
+            console.log("Listening on port: 8001"); 
+            app.listen(8001);
+        });
+    });
 });
